b7d837d39fdd9e8c9babf469fdc788378037be4c
19Aleksandra95 <polar.heder@gmail.com>
2024-07-22 11:55:27 +0200

    commit

--- app/components/AddToFavorite.tsx
+++ app/components/AddToFavorite.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default function AddToFavorite() {
   return (
     <div>
-        <button onClick={() => console.log('Click')}>Add to Favorite</button>
+        <button className='btn btn-primary' onClick={() => console.log('Click')}>Add to Favorite</button>
     </div>
   )
 }
--- app/components/FavoriteCard.tsx
+++ app/components/FavoriteCard.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import styles from './FavoriteCard.module.css'
 import AddToFavorite from "./AddToFavorite";
 
 export default function FavoriteCard() {
   return (
-    <div className={styles.card}>
+    <div>
       <AddToFavorite/>
     </div>
   );
